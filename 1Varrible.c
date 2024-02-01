f520b2ebdebc524849a81d37b7fbd5034def8fcc
WHSD096 <136942862+WishMaur@users.noreply.github.com>
2024-02-01 21:24:37 +0530

    VARRIBLES
    CH01 VARRIBLE, CONSTANTS, KEYWORDS AND DATA TYPES + INPUT OUTPUT

---
+++ 1Varrible.c
@@ -0,0 +1,37 @@
+/*
+VARIABLE:-
+A name given to a memory location which store some data
+Declear by ... type variable_name= value;
+*/
+
+/*
+RULE OF DEFINING A VARIABLE IN C:- 
+Variable are case Sensitive(a,A,b,B etc)
+It can contain alphabets, digit, underscore(_);
+A varible can start with an alphabet,underscore only
+Cant start with a digit;
+No comma,whitespace, symbol & reserved keywords allow;
+*/
+
+// In C 32 words are Reserved keywords means... dont use as like varible
+// like int float  for if etc 
+
+
+
+#include<stdio.h>
+int main(){
+    int number=4; // number is assigned "4"
+    char star='*';
+    float a=5.7;
+
+    int age=17;
+    age=18;
+
+    int b=12;
+    int B=4;
+
+    int _age=19;
+
+    int final_price=95;
+
+}
