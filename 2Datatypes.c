d4259f9532628b89f97205f9bc2d3aa61fa9de57
WHSD096 <136942862+WishMaur@users.noreply.github.com>
2024-02-02 09:43:02 +0530

    CH01 VARRIBLE, DATATYPE ,CONSTANTS, KEYWORDS
    CH01 VARRIBLE, CONSTANTS, KEYWORDS AND DATA TYPES + INPUT OUTPUT

---
+++ 2Datatypes.c
@@ -0,0 +1,27 @@
+/*
+BASIC DATA TYPE:- int, float , double, char ...
+DERIVED DATA TYPE :- array, pointer, structure, union ...
+ENUMERATION DATA TYPE:- emum
+VOID DATA TYPE:- void
+*/
+
+#include<stdio.h>
+int main(){
+    int a=10;
+    int b=3;
+    float c=3.14;
+    char d='A';
+    int e=12+8;
+    int f=a+b;
+
+    printf("The value of a is %d \n ", a);
+    printf("The value of c is %f \n ", c);
+    printf("The value of d is %c \n ", d);
+    printf("The value of e is %d \n ", e);
+    printf("The value of f is %d \n ", f);
+    printf("some of a and b is %d \n ", a+b);
+    
+
+
+return 0;
+}
