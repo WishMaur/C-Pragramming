bf86420d7110b255d3be6a60f4906d8f4e64e7bb
WHSD096 <136942862+WishMaur@users.noreply.github.com>
2024-02-09 09:14:55 +0530

    CH02 INSTRUCTIONS AND OPERATORS
    CH02 INSTRUCTIONS AND OPERATORS

---
+++ 2RelationalOperator.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int a,b,c;
+    a=4;
+    b=6;
+    c=4;
+
+    printf("a==b %d\n",a==b); //condition false ...return 0
+    printf("a==c %d",a==c);  //condition true ...return 1
+
+    
+    /*
+    Relational Operators Used In If Condition & Loop..
+    a==b
+    a!=b
+    a<b
+    a>b
+    a<=b
+    a=>b
+    */
+
+   return 0;
+    
+}
